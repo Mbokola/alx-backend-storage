9fcf292756c8e9b5d5079e78439af88db7935d69
Mbokola <tonypeaks@gmail.com>
2023-12-19 21:33:07 +0000

    Log stats

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -5,4 +5,4 @@
 def schools_by_topic(mongo_collection, topic):
     """ function that returns the list of school having a specific topic """
 
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This function provides some stats about Nginx logs stored in MongoDB """
+
+
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    status_check = nginx_collection.count_documents({"method": "GET",
+                                                     "path": "/status"})
+    print(f"{nginx_collection.count_documents({})} logs\nMethods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE", "/status"]:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}") if method != "/status" else print(
+            f"{status_check} status check")
