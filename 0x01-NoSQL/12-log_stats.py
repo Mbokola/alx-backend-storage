be95317d78f98d4268126bbb1cc80ee9efdecde4
Mbokola <tonypeaks@gmail.com>
2023-12-19 21:34:10 +0000

    Requirements

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ This function provides some stats about Nginx logs stored in MongoDB """
 
 
 from pymongo import MongoClient
-list_all = __import__('8-all').list_all
-
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
